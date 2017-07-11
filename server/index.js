a1f5880d7b054645c79cb02c79ed4b9bbe63d2b7
Unknown <Frank_Schutte@hotmail.com>
2017-07-11 16:51:02 +0200

    Add cors

--- server/index.js
+++ server/index.js
@@ -10,6 +10,13 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use('/api', api);
 
 app.get('/*', function (req, res) {
