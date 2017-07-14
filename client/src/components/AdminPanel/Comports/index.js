05066bededf38bc0dd293ccd40e2977c8d573b21
Frank Schutte <Frank_Schutte@hotmail.com>
2017-07-14 18:22:59 +0200

    Add admin panel views

---
+++ client/src/components/AdminPanel/Comports/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css';
+
+const Comports = () => {
+    return (
+        <section>
+            <h2>My comports</h2>
+        </section>
+    );
+};
+
+export default Comports;
---
+++ client/src/components/AdminPanel/Devices/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './style.css';
+
+const Devices = () => {
+    return (
+        <section>
+            <h2>My devices</h2>
+        </section>
+    );
+};
+
+export default Devices;
