8b20ac86799f49df2bfd1089510c5ba282d090d4
Frank Schutte <Frank_Schutte@hotmail.com>
2017-07-23 21:50:53 +0200

    Just make sure to dispatch actions to arduino

--- server/interfaces/arduino/index.js
+++ server/interfaces/arduino/index.js
@@ -13,40 +13,14 @@ const Arduino = {
             callback(err, comports);
         });
     },
-    learnCommand: function (protocol, callback) {
-        if (port === undefined || !port.isOpen()) {
-            return callback('Make sure to set the comport');
-        }
-
-        const action = {
-            type: 'LEARN_COMMAND',
-            protocol: protocol
-        };
-
-        port.write(JSON.stringify(action), function (err) {
-            if (err) {
-                console.error(err);
-                return callback('Unable to write action to Arduino');
-            }
-
-            port.once('data', function (data) {
-                const parsedData = JSON.parse(data);
-                const err = parsedData.err;
-                const command = parsedData.command;
-
-                callback(err, command);
-            });
-        });
-    },
     setComport: function (comName, callback) {
         initializePort(comName, callback);
     },
-    toggleDevice: function (action, callback) {
+    performAction: function (action, callback) {
         if (port === undefined || !port.isOpen()) {
             return callback('Make sure to set the comport');
         }
 
-        action.type = 'SEND_COMMAND';
         port.write(JSON.stringify(action), function (err) {
             if (err) {
                 console.error(err);
@@ -54,11 +28,7 @@ const Arduino = {
             }
 
             port.once('data', function (data) {
-                const parsedData = JSON.parse(data);
-                const err = parsedData.err;
-                const success = parsedData.success;
-
-                callback(err, success);
+                callback(JSON.parse(data));
             });
         });
 
